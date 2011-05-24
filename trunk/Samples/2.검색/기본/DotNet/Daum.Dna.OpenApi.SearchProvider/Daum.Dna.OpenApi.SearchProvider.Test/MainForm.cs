0a2e0531ec3199c173406a7fe5251e98c5cc6e50
jeongzu@gmail.com <jeongzu@gmail.com@13a6c53b-0901-81b1-03f3-01ba54ed1325>
2011-05-24 10:52:41 +0000

    dotnet 초기샘플구현

---
+++ trunk/Samples/2.검색/기본/DotNet/Daum.Dna.OpenApi.SearchProvider/Daum.Dna.OpenApi.SearchProvider.Test/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Daum.Dna.OpenApi.SearchProvider.Responses;
+
+namespace Daum.Dna.OpenApi.SearchProvider.Test
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            BlogData d = OpenApiProvider.RequestBlogApi("7fd190dd37f0ad9d15cbfd69e6d1aa3320623256", "apple", 10, 1, "accu", "rss", "");
+            
+        }
+    }
+}
